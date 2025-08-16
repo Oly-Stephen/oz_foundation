5e69e8a4b1d774add8b951f340b6c5dea708c7ab
Oly-Stephen <stephenoluwayanmi@gmail.com>
2025-08-16 23:10:40 +0200

    logo adjustment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,11 +17,7 @@ const Hero: React.FC = () => {
           <div className="space-y-6 lg:space-y-8">
             <div className="space-y-4">
               <div className="inline-flex items-center px-3 py-2 lg:px-4 lg:py-2 rounded-full bg-yellow-100 text-yellow-700 text-xs lg:text-sm font-medium">
-                <img 
-                  src="/oz_logo_black.png" 
-                  alt="OZ Foundation Logo" 
-                  className="w-4 h-4 lg:w-5 lg:h-5 mr-2" 
-                />
+               
                 <span>The OZ Foundation</span>
               </div>
               
